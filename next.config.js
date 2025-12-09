59145ab0d5fe8619662396e13ad6dfe3f84111f2
jengerred <jengerred@aol.com>
2025-12-09 15:58:14 -0500

    fix: update next.config.js to fix build warnings

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ['mongoose'],
-  },
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/**',
+      },
+    ],
   },
   webpack(config) {
     config.experiments = {
